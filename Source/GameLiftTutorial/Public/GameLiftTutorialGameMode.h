db0d5fdba55c26fe50dec7d39624caf90063c260
SuperNimbus-Sam <samuel@supernimbus.net>
2023-07-31 12:34:06 +0100

    additional comments

--- Source/GameLiftTutorial/Public/GameLiftTutorialGameMode.h
+++ Source/GameLiftTutorial/Public/GameLiftTutorialGameMode.h
@@ -9,7 +9,7 @@
 #include "GameLiftTutorialGameMode.generated.h"
 
 
-// Custom Log category
+// Custom Log categories
 DECLARE_LOG_CATEGORY_EXTERN(GameLift, Log, All);
 
 DECLARE_LOG_CATEGORY_EXTERN(BrainCloud, Log, All);
@@ -25,23 +25,26 @@ public:
 	AGameLiftTutorialGameMode();
 
 protected:
+	// GameModeBase override functions we will use
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaSeconds) override;
 	virtual void OnPostLogin(AController* NewPlayer) override;
 	virtual void Logout(AController* Exiting) override;
 
 private:
+	// Initialisation functions
 	void InitGameLift();
 	void InitBrainCloud();
 
+	// Shutdown functions
 	void ShutDownGameLift() const;
 	void ShutDownBrainCloud() const;
 	void ShutDownServer(const FString& reason) const;
 
+	// Empty server timer functions
 	void StartTimer();
 	void CheckDisconnectedPlayers();
 	void EmptyServerTimeout() const;
-
 
 	// GameLift sdk reference and parameters that the sdk is initialised with
 	FGameLiftServerSDKModule* gameLiftSdkModule;
@@ -60,13 +63,13 @@ private:
 	FString serverSecret = "";
 	FString lobbyId = "";
 
-	// Flag to start running BC s2s callbacks
-	bool brainCloudReady = false;
-
 	// BrainCloud sdk reference
 	TSharedPtr<UBrainCloudS2S> pS2S;
 
-	// GameSession Tracking
+	// Flag to start running BC s2s callbacks
+	bool brainCloudReady = false;
+
+	// Player tracking
 	int playerCount = 0;
 	int secondsToShutDownEmptyServer = 10;
 	int timerCounter = 0;
